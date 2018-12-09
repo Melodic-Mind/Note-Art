547bf3f79ce3e56bd94d408e1413320119e37281
Sean Dvir <seandvir12@gmail.com>
2018-12-09 18:48:55 +0200

    Edited function names

--- src/Piano.js
+++ src/Piano.js
@@ -1,6 +1,5 @@
 import { Note, allNotes, noteDurations, firstToUpper } from '.'
 
-<<<<<<< HEAD
 /**
  * @class
  */
@@ -9,15 +8,11 @@ export class Piano {
      * @classdesc Piano instance - creates a piano that can play any note from any octave for any duration.
      * initializes the piano object with all piano notes.
      */
-=======
-class Piano {
->>>>>>> 0c418e50ea9273d341f0399cc468c62df27fa305
     constructor() {
         this.piano = new Map()
         for (let i = 1; i <= 7; i++)
             for (const n of allNotes)
                 for (let l in noteDurations)
-<<<<<<< HEAD
                     this.setKey(n, i, l)
         const otherNotes = [{ note: 'A', octave: 0 }, { note: 'A#', octave: 0 }, { note: 'Bb', octave: 0 }, { note: 'B', octave: 0 }, { note: 'C', octave: 8 }]
         for (let l in noteDurations)
@@ -47,18 +42,3 @@ class Piano {
      */
     note(note) { return this.piano.get(firstToUpper(note)) }
 }
-=======
-                    this.piano.set(n + i + l, new Note({ note: n, octave: i, duration: l, instrument: 'Piano' }))
-        for (let l in noteDurations) {
-            this.piano.set('A0' + l, new Note({ note: 'a', octave: 0, duration: l, instrument: 'Piano' }))
-            this.piano.set('Bb0' + l, new Note({ note: 'bb', octave: 0, duration: l, instrument: 'Piano' }))
-            this.piano.set('B0' + l, new Note({ note: 'B', octave: 0, duration: l, instrument: 'Piano' }))
-            this.piano.set('C8' + l, new Note({ note: 'C', octave: 8, duration: l, instrument: 'Piano' }))
-        }
-    }
-
-    note(note) { return this.piano.get(firstToUpper(note)) }
-}
-
-export { Piano }
->>>>>>> 0c418e50ea9273d341f0399cc468c62df27fa305
