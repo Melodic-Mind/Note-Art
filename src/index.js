910b166c6f0b5d8e4fc80906cdcf8f8155de7377
Seanitzel <seandvir12@gmail.com>
2019-10-15 14:50:56 +0300

    updated all dependencies

--- src/index.js
+++ src/index.js
@@ -14,3 +14,23 @@ export *                       from './validation'
 export {MusicTheoryStructures} from './resources/MusicTheoryStructures'
 export ScorePlayer             from './transport/ScorePlayer'
 export {lib}
+
+//@todo add good docs using vuepress - like this:
+/*
+ * @vuepress
+ * ---
+ * title: Your custom title
+ * headline: You custom headline
+ * ---
+ * ::: tip
+ * This is a tip
+ * :::
+ *
+ * ::: warning
+ * This is a warning
+ * :::
+ *
+ * ::: danger omg
+ * This is a dangerous warning
+ * :::
+ */
