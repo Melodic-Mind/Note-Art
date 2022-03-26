18e41f4745a8fa41fe59f5b7ec755c002536f0ed
Seanitzel <seandvir12@gmail.com>
2022-03-26 15:58:54 +0300

    Update docs

--- src/types.ts
+++ src/types.ts
@@ -10,13 +10,14 @@ type SharpAccidental = '#' | 'x' | '#x';
 
 export type Accidental = '' | FlatAccidental | SharpAccidental;
 
+export type PitchClass = 'C' | 'C#' | 'Db' | 'D' | 'D#' | 'Eb' | 'E' | 'F' | 'F#' | 'Gb' | 'G' | 'G#' | 'Ab' | 'A' | 'A#' | 'Bb' | 'B';
 
 export type Octave = number;
 
 export type FlatPitchClass = `${PitchClassLetter}${FlatAccidental}`;
 export type SharpPitchClass = `${PitchClassLetter}${SharpAccidental}`;
 
-export type PitchClass = PitchClassLetter | FlatPitchClass | SharpPitchClass;
+// export type PitchClass = PitchClassLetter | FlatPitchClass | SharpPitchClass;
 
 export type PureFlatPitchClass = 'C' | 'Db' | 'D' | 'Eb' | 'E' | 'F' | 'Gb' | 'G' | 'Ab' | 'A' | 'Bb' | 'B';
 export type PureSharpPitchClass = 'C' | 'C#' | 'D' | 'D#' | 'E' | 'F' | 'F#' | 'G' | 'G#' | 'A' | 'A#' | 'B';
